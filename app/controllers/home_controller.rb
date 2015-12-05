f6207b0dd957eeef70ddb78ae5672f65fbe1368f
zeljko <zeljko@z-ware.fi>
2015-12-05 10:32:12 +0200

    flexible url configuration
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,22 @@
 require 'oauth2'
 
 class HomeController < ApplicationController
+  MyGuessOfIntranetIp = `ifconfig`.scan(/inet ([\.\d]+)/).flatten.find { |x| x != '127.0.0.1' }
+
   APPLICATION_ID = '32258b408332a9504bf3d048a5879ed3b9978474d408b2f3c9bab1cb8a03cda3'
   APPLICATION_SECRET = '96a730fb7a806fc1f6cb8e52b7d1f40b5314126978dad977bfcb5682c922760c'
 
-  CALLBAK_URL = 'http://192.168.6.108:3001/auth/'
-  PROVIDER_SITE = 'http://192.168.6.108:3000/'
+  CALLBAK_URL = "http://#{MyGuessOfIntranetIp}:3001/auth/"
+  PROVIDER_SITE = "http://#{MyGuessOfIntranetIp}:3000/"
 
-  def rum(resource_path)
+  def rum(resource_path, method: :get, post_body: {})
     @oauth = OAuth2::Client.new APPLICATION_ID, APPLICATION_SECRET,
                                 site: PROVIDER_SITE # , token_method: :post
+
     begin
       if session[:rum_token]
         @token = OAuth2::AccessToken.new @oauth, session[:rum_token]
-        response = @token.get resource_path
+        response = @token.send method, resource_path, body: post_body
         return JSON.parse response.body
       end
     rescue OAuth2::Error # rubocop:disable HandleExceptions
